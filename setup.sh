7478f020a5ce08f400d91ba2f8dee51a1f3bedf5
Roman <roman.bitetti.com>
2021-04-20 00:20:47 +0200

    :(

--- setup.sh
+++ setup.sh
@@ -9,33 +9,40 @@ if [ $nproc -lt 2 ]; then
 	exit 1
 fi
 
-sudo chown $USER /var/run/docker.sock > /dev/null 2>&1
+sudo chown $USER /var/run/docker.sock #> /dev/null 2>&1
 sudo systemctl stop nginx
 
-wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+minikube stop #> /dev/null 2>&1
+minikube delete #> /dev/null 2>&1
+docker stop $(docker ps -a -q) #> /dev/null 2>&1
+docker rm $(docker ps -a -q) #> /dev/null 2>&1
+docker rmi $(docker images -a -q) #> /dev/null 2>&1
+
+wget -q https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 chmod +x minikube-linux-amd64
 sudo mv minikube-linux-amd64 /usr/local/bin/minikube
 
-minikube stop > /dev/null 2>&1
-minikube delete > /dev/null 2>&1
 printf "\e[32;1mDone\n\n\e[0m"
 
 printf "\e[33;1m1 -- Installing Minikube\n\e[0m"
 minikube start --driver=docker
 eval $(minikube docker-env)
+IP="$(kubectl get node -o=custom-columns='DATA:status.addresses[0].address' | sed -n 2p)"
+sed -i 's/IP/'$IP'/g' srcs/k8s/metallb/config-map.yaml
+sed -i 's/IP/'$IP'/g' srcs/wordpress/srcs/start.sh
 printf "\e[32;1mDone\n\n\e[0m"
 
 printf "\e[33;1m2 -- Enabling Addons\n\e[0m"
-minikube addons enable metrics-server
 minikube addons enable dashboard
+minikube addons enable metrics-server
 minikube addons enable metallb
 printf "\e[32;1mDone\n\n\e[0m"
 
 printf "\e[33;1m2 -- Building Images\n\e[0m"
-docker build --network=host -t ft_services/nginx srcs/nginx/
-docker build --network=host -t ft_services/mysql srcs/mysql/
-docker build --network=host -t ft_services/wordpress srcs/wordpress/
-docker build --network=host -t ft_services/phpmyadmin srcs/phpmyadmin/
+docker build --quiet --network=host -t ft_services/nginx srcs/nginx/
+docker build --quiet --network=host -t ft_services/mysql srcs/mysql/
+docker build --quiet --network=host -t ft_services/wordpress srcs/wordpress/
+docker build --quiet --network=host -t ft_services/phpmyadmin srcs/phpmyadmin/
 # docker build -t ftps_ft-services srcs/ftps/
 # docker build -t influxdb_ft-services srcs/influxdb/
 # docker build -t telegraf_ft-services srcs/telegraf/
@@ -48,4 +55,7 @@ kubectl apply -f srcs/k8s/mysql/
 kubectl apply -f srcs/k8s/wordpress/
 kubectl apply -f srcs/k8s/phpmyadmin/
 
-#minikube dashboard &
+sed -i 's/'$IP'/IP/g' srcs/k8s/metallb/config-map.yaml
+sed -i 's/'$IP'/IP/g' srcs/wordpress/srcs/start.sh
+
+minikube dashboard &
--- srcs/wordpress/srcs/start.sh
+++ srcs/wordpress/srcs/start.sh
@@ -6,7 +6,7 @@ mkdir -p /var/www/wordpress/
 
 wp core download --locale=en_US --path=/var/www/wordpress/
 wp config create --skip-check --allow-root --path=/var/www/wordpress/ --dbname=wordpress --dbuser=admin --dbpass=password --dbhost=mysql
-wp core install --allow-root --path=/var/www/wordpress/ --url=https://192.168.1.250:5050 --title=ft_services --admin_user=admin --admin_password=password --admin_email=admin@ft_services.com
+wp core install --allow-root --path=/var/www/wordpress/ --url=https://IP:5050 --title=ft_services --admin_user=admin --admin_password=password --admin_email=admin@ft_services.com
 
 wp user create Elon elon@spacex.com --role=author ;
 wp user create Jeff jeff@amazon.com --role=contributor;
